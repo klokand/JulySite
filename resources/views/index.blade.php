7ed7bd2b01f5682fc69006180c1c08b3afdc86ec
klokand <xjz520223@gmail.com>
2015-08-17 12:39:48 +1000

    fixing migrate

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,11 @@
 ﻿@extends('app')
 
 @section('content')
-@if ($page->isEmpty())
-    Nothing Here
-@else 
     @include('layout.slider')
 	@include('layout.projects')
 	@include('layout.offers')
 	@include('layout.teamInfo')
 	@include('layout.quotes')
 	@include('layout.clients')
-@endif
-
 
 @endsection
