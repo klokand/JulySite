32491214b5cfe95e22ef5e24196e8006b66c8ccc
klokand <xjz520223@gmail.com>
2015-08-18 20:56:14 +1000

    fix News Rules

--- app/News.php
+++ app/News.php
@@ -13,7 +13,7 @@ class News extends Model {
 		'content'=>'required|min:10',
 		'author'=>'required|min:2',
 		'title'=>'required|min:2',
-		'coverImage'=>'image|max:2000',
+		'coverImage'=>'required|image|max:2000',
 	);
 	public function scopeNews($query){
 		return $query->where('types','=','news')->orderBy('created_at', 'asc');
